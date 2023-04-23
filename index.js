b6aa9a784abda2b2bd2a5a000482941e1f800412
Favour Oghenekowho <gh.oghene@gmail.com>
2023-04-23 20:26:32 +0100

    init

---
+++ index.js
@@ -0,0 +1,21 @@
+// const hello = 'hello world';
+// console.log(hello);
+
+// const fs = require('fs');
+
+/////////////////////SYNCHRONOUS WAY/////////////////////////////
+// console.log('before execution');
+// const data = fs.readFileSync('./text/inde.txt', 'utf-8');
+// console.log(data);
+// console.log('after execution');
+
+// const text = 'newton is a backend developer';
+
+// fs.writeFileSync('./text/newton.txt', text, 'utf-8');
+
+/////////////////////ASYNCHRONOUS WAY////////////////////////////
+// console.log('before execution');
+// fs.readFile('./text/inde.txt', 'utf-8', (err, data) => {
+//   console.log(data);
+// });
+// console.log('after execution');
